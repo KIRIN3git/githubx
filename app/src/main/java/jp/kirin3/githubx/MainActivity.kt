33eb188ffbb3d210dfb0487edd48255b8efe7025
KIRIN3 <scomputer13@gmail.com>
2020-05-17 22:04:05 +0900

    ２回目コミット

--- app/src/main/java/jp/kirin3/githubx/MainActivity.kt
+++ app/src/main/java/jp/kirin3/githubx/MainActivity.kt
@@ -12,5 +12,26 @@ class MainActivity : AppCompatActivity() {
 
         val aaa = ""
         val bbb = ""
+
+        val ccc = ""
+        val xxx = "testa"
+
+        val aaa1 = ""
+        val bbb1 = ""
+
+        val ccc1 = ""
+        val xx1x = "testa"
+
+        val a2aa = ""
+        val bb3b = ""
+
+        val c21cc = ""
+        val x21xx = "testa"
+
+        val a21aa = ""
+        val b321bb = ""
+
+        val cc312c = ""
+        val x3332xx = "testa"
     }
 }
