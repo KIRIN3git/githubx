bd99f215078d041f7354c4a8b0a79b3500ad2058
KIRIN3git <scomputer13@gmail.com>
2020-05-17 22:59:42 +0900

    Revert "Revert "Feature b""

--- app/src/main/java/jp/kirin3/githubx/MainActivity.kt
+++ app/src/main/java/jp/kirin3/githubx/MainActivity.kt
@@ -1,7 +1,7 @@
 package jp.kirin3.githubx
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +11,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val aaa = ""
-
-
         val bbb = ""
+
+        val ccc = ""
+        val xxx = "testa"
+
+        val aaa1 = ""
+        val bbb1 = ""
+
+        val ccc1 = ""
+        val xx1x = "testa"
+
+        val a2aa = ""
+        val bb3b = ""
+
+        val c21cc = ""
+        val x21xx = "testa"
+
+        val a21aa = ""
+        val b321bb = ""
+
+        val cc312c = ""
+        val x3332xx = "testa"
     }
 }
